14e1962ee91200965567573c790a987dce63b587
fiqsss <sarofiqs@gmail.com>
2024-04-21 16:23:24 +0700

    add link

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,43 +1,39 @@
-import git from '../assets/svg/github.svg'
-import Yt from '../assets/svg/youtube.svg'
-import Porto from '../assets/svg/portfolio.svg'
-import Instagram from '../assets/svg/instagram.svg'
-
-
 export default function Body() {
     return (
         <div className="md:w-[653px] w-[390px] md:px-0 px-4 h-[561] mb-10 z-20 ">
-            <p className="text-center">Web Develoment</p>
+            <p className="text-center">Web Aplication</p>
             <div className='relative z-50'>
-                <a href="https://arofiqs.my.id">
-                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-between items-center">
-                        <img className='w-7' src={Porto} alt="" />
-                        <span className="text-[15px] md:text-[20px]">Portofolio</span>
-                        <img className='w-7' src={Porto} alt="" />
+                <a href="https://fiqssspos.fixapp.my.id">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">Aplikasi Kasir</span>
+                    </button>
+                </a>
+                <a href="https://ekatalog.fixapp.my.id">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">E-katalog Product Networking</span>
+                    </button>
+                </a>
+                <a href="https://datasiswa.fixapp.my.id">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">Data Siswa</span>
+                    </button>
+                </a>
+                <a href="https://inventory.fixapp.my.id">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">Inventory</span>
                     </button>
                 </a>
             </div>
             <div className='relative z-50'>
-                <p className="text-center">Social Media</p>
-                <a href="https://www.instagram.com/aro_fiqs/">
-                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-between items-center">
-                        <img className='w-7' src={Instagram} alt="" />
-                        <span className="text-[15px] md:text-[20px]">Instagram</span>
-                        <img className='w-7' src={Instagram} alt="" />
+                <p className="text-center">Landing Page</p>
+                <a href="https://simpleclothindustries.fixwebstudio.com">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">simpleclothindustries.com</span>
                     </button>
                 </a>
-                <a href="https://www.youtube.com/channel/UCxDVXoNzjiEfFDF9yWqwC3Q">
-                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-between items-center">
-                        <img className='w-7' src={Yt} alt="" />
-                        <span className="text-[15px] md:text-[20px]">Youtube</span>
-                        <img className='w-7' src={Yt} alt="" />
-                    </button>
-                </a>
-                <a href="https://github.com/Fiqsss">
-                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-between items-center">
-                        <img className='w-7' src={git} alt="" />
-                        <span className="text-[15px] md:text-[20px]">Github</span>
-                        <img className='w-7' src={git} alt="" />
+                <a href="https://istanakubahpati.com">
+                    <button className="btn px-5 text-xl bg-[#8BD3DD] text-white w-full h-[51px] md:h-[67px] shadow-custom border-4 border-[#001858] rounded-xl my-5 hover:shadow-none transition-all duration-300 ease-in-out flex justify-center items-center">
+                        <span className="text-[15px] md:text-[20px]">istanakubahpati.com</span>
                     </button>
                 </a>
             </div>
